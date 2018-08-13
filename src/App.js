3a4705a6a575ce49a2506e02ca5c78ef15a6f3e7
craig <grendel@shmoo.com>
2018-08-13 13:56:20 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import CartItems from './components/CartItems'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header/>
+      <CartItems />
+
+      <Footer/>
+    </div>);
+  }
+}
+
+export default App;
